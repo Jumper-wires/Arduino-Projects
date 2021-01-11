c6093c2cb66eb61ca5081d37690ed0502dfebae6
Hardik Chopra <hardikchopra242@gmail.com>
2021-01-11 17:32:52 +0530

    [New] added Beep_with_Distance

---
+++ Buzzer/Beep_with_Distance/Beep_with_Distance.ino
@@ -0,0 +1,52 @@
+// Buzzer Beeps faster as you get close to it!
+
+const int buzzerPin = 8;
+const int trigPin = 12;
+const int echoPin = 13;
+const float speedLight = 0.0343;//in cm/microseconds
+
+float duration;  //Stores time taken by the signal to return
+float distance;  //Stores the distance signal travelled
+float delayBuzzer; //For changing the frequency of buzzer
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin,INPUT);
+}
+
+void loop() {
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);      //Wait for small time to make sure that trigPin is at LOW
+  
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);      //Sent signal for 10 Micro Seconds
+  digitalWrite(trigPin, LOW);
+
+   duration = pulseIn(echoPin, HIGH);  
+   distance = (duration * speedLight)/2;  
+
+// Changing buzzer frequency depending on the value of distance
+  if(distance > 500){ 
+   delayBuzzer = 200; 
+  }else if(distance<=500 && distance>100){
+    delayBuzzer = 100; 
+  }else if(distance<=30 && distance>10){
+    delayBuzzer = 50; 
+  }else{
+    delayBuzzer = 20; 
+  }
+  
+  digitalWrite(buzzerPin, LOW);
+  delay(delayBuzzer);
+  digitalWrite(buzzerPin,HIGH);
+  delay(delayBuzzer);
+    
+  // Open Serial-Monitor to see the output
+  Serial.print("Distance : ");
+  Serial.println(distance);
+  Serial.print(" cm");
+}
